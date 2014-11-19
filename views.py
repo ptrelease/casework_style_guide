c09f0e611d89361cb69d5dcf77da3a13d85abfb6
treacle74 <ptrelease@gmail.com>
2014-11-19 14:35:18 +0000

    added requirements.txt

--- views.py
+++ views.py
@@ -1,6 +1,12 @@
 from __init__ import app
 from flask import render_template
+import os
+
 
 @app.route('/')
 def index():
   return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 5010)))
+
