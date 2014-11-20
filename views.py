6e94fcbd0b3b06299577332405d1ddaa30f2f8b2
treacle74 <ptrelease@gmail.com>
2014-11-20 15:16:11 +0000

    Added some templates with some beginning principles we discussed early

--- views.py
+++ views.py
@@ -8,6 +8,21 @@ app = Flask(__name__)
 def index():
   return render_template("index.html")
 
+@app.route('/platforms')
+def platforms():
+    return render_template("platforms.html")
+
+@app.route('/whatwethinktheuserswant')
+def what_we_think_the_users_want():
+    return render_template("what_we_think_the_users_want.html")
+
+@app.route('/usertypes')
+def user_types():
+    return render_template("user_types.html")
+
+
+
+
 if __name__ == '__main__':
     app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 5010)))
 
